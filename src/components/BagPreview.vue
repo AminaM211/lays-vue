80423ce4f89e62c56df4955faf185463d12c30dc
AminaM211 <148769375+AminaM211@users.noreply.github.com>
2026-01-01 19:05:36 +0100

    fix: update bagpreview

--- src/components/BagPreview.vue
+++ src/components/BagPreview.vue
@@ -25,7 +25,9 @@ export default {
       0.1,
       100
     )
+    camera.position.set(0, 2, 4)
     camera.lookAt(0, 1.8, 0)
+
     camera.fov = 55
     camera.position.set(0, 1.2, 2)
     camera.updateProjectionMatrix()
@@ -74,7 +76,7 @@ export default {
       objLoader.load("/assets/chips-bag-obj/bag.obj", (bagMesh) => {
         bagMesh.scale.set(0.5, 0.5, 0.6)
         bagMesh.position.y = 0.9
-        bagMesh.rotation.y = 0.7
+        bagMesh.rotation.y = 0.6
 
         scene.add(bagMesh)
 
