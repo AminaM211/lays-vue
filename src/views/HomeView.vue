27d1672badd268c39aaef4fb81a198f0e89a9f8d
AminaM211 <148769375+AminaM211@users.noreply.github.com>
2026-01-01 17:59:02 +0100

    fix: update image paths

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -170,7 +170,7 @@ export default {
 
       if (bag.backgroundPreset) {
         return {
-          backgroundImage: `url(/${bag.backgroundPreset}-bg.png)`,
+          backgroundImage: `url(assets/${bag.backgroundPreset}-bg.png)`,
           backgroundSize: "cover",
           backgroundPosition: "center"
         }
