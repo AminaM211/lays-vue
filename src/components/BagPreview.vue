5db3254a87ed31c73122fd141224a6954756fb4d
AminaM211 <148769375+AminaM211@users.noreply.github.com>
2026-01-01 19:31:27 +0100

    fix: update bagpreview

--- src/components/BagPreview.vue
+++ src/components/BagPreview.vue
@@ -82,7 +82,7 @@ export default {
       objLoader.load("/assets/chips-bag-obj/bag.obj", (object) => {
         bagMesh = object
         bagMesh.scale.set(0.5, 0.5, 0.6)
-        bagMesh.position.set(0.3, 2.4, 0)
+        bagMesh.position.set(0.3, 4, 0)
 
         scene.add(bagMesh)
 
